959e0d47a207dcf348413782316aa6c93b19c5b0
Mohzis1 <isangmoses0@gmail.com>
2024-07-12 05:22:06 +0100

    Still modify

--- Skinsphere-App/src/layouts/AllProduct.tsx
+++ Skinsphere-App/src/layouts/AllProduct.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import CleanserProduct from '../component/CleanserProduct.tsx'
 import SerumProduct from '../component/SerumProduct.tsx'
 import MoisturizerProduct from '../component/MoisturizerProduct.tsx'
--- Skinsphere-App/src/main.tsx
+++ Skinsphere-App/src/main.tsx
@@ -6,14 +6,10 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-
-import App from './component/App';
 import Cleanser from './component/CleanserProduct.tsx';
 import Serum from './component/SerumProduct.tsx';
 import Moisturizer from './component/MoisturizerProduct.tsx';
 import FaceOil from './component/FaceOilProduct.tsx';
-import Navbar from "./component/navbar.tsx";
-import Footer from "./component/Footer.tsx";
 import HomeLayout from "./layouts/HomeLayout.tsx"
 import AllProduct from "./layouts/AllProduct.tsx"
 
